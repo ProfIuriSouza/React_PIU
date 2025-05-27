0df69ddedb47506c876f03df45177c73d8fabffe
Iuri Souza <pedro.iuri@ifrn.edu.br>
2025-05-27 13:59:45 -0300

    dropdown

--- 03_PROPS/props/src/App.jsx
+++ 03_PROPS/props/src/App.jsx
@@ -4,15 +4,19 @@ import Children from './componentes/Children'
 import './App.css'
 import Texto from './componentes/Texto'
 import foto from './assets/nature.jpg'
+import Dropdown from './componentes/DropDown'
 
 function App() {
-let aluno = "ana"
+//let aluno = "ana"
+
+const menus=['Link1','Link2','Link3','Link4', 'Link5'];
+const nomes=['Menu1','Menu2','Menu3','Menu4', 'Menu5'];
 
   return (
     <>
       {/* <Props nome={aluno} newname={"testetestetdddfs"} atributo={"aluno"}/>
       <Props nome={"peofessort"}/>
-      <Props name={"name"} imagem={foto}/> */}
+      <Props name={"name"} imagem={foto}/>
       <Card />
       <br />
       <Card />
@@ -24,7 +28,10 @@ let aluno = "ana"
       <Children>
         <Texto/>
         <Card />
-      </Children>
+      </Children> */}
+
+        <Dropdown titulo={nomes} lista={menus}/>
+
     </>
   )
 }
