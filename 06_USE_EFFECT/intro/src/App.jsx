b74e86b597db42e35c86bb3318c7ea3fda973d13
unknown <pedro.iuri@ifrn.edu.br>
2025-07-16 09:53:45 -0300

    useEffect

---
+++ 06_USE_EFFECT/intro/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CountClicks from './componentes/CountClicks'
+import EffectAPI from './componentes/EffectAPI'
+
+function App() {
+
+  return (
+    <>
+      {/* <CountClicks/> */}
+      <EffectAPI />
+    </>
+  )
+}
+
+export default App
