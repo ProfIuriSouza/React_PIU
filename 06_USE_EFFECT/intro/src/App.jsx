b4d7a24d8e64b58dd1e3ee41aad4eab524e04fb9
unknown <pedro.iuri@ifrn.edu.br>
2025-07-16 10:11:04 -0300

    arquivo db.json

--- 06_USE_EFFECT/intro/src/App.jsx
+++ 06_USE_EFFECT/intro/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import CountClicks from './componentes/CountClicks'
 import EffectAPI from './componentes/EffectAPI'
+import RepoJson from './componentes/RepoJson'
+
 
 function App() {
 
   return (
     <>
       {/* <CountClicks/> */}
-      <EffectAPI />
+      {/* <EffectAPI /> */}
+      <RepoJson />
+      
     </>
   )
 }
